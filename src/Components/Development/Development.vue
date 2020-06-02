49398ecb193eff6ae21014be2eb8363ec535bfe0
Mgo <mgotm13@gmail.com>
2020-06-02 23:29:35 +0400

    Custom Directive

--- src/Components/Development/Development.vue
+++ src/Components/Development/Development.vue
@@ -1,13 +1,34 @@
 <template>
   <div class="container">
-    <div v-awesome></div>
+    <div>{{username}}</div>
+    <div v-awesome.red.big="text"></div>
+    <div v-awesome.blue.small="text"></div>
+    <div v-user-name="text"></div>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "Development"
+  export default {
+    name: "Development",
+    data() {
+      return {
+        text:'sss',
+        username:''
+      }
+    },
+    mounted() {
+      setTimeout( () => {
+          this.username = 'ssssss';
+      })
+    },
+    directives: {
+      'user-name': {
+        bind(element, binding, vNode) {
+          element.innerHTML = binding.value;
+        },
+      }
     }
+  }
 </script>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,25 @@ import App from './App.vue';
 
 export const bus = new Vue();
 
-Vue.directive('awesome',{
-      bind(element,binding,vNode){
-         element.innerHTML = 'Hello World';
-      }
+Vue.directive('awesome', {
+  bind(element, binding, vNode) {
+    element.innerHTML = 'Hello World';
+    element.style.color = (binding.modifiers.red) ? 'red' : 'blue';
+    element.style.fontSize = (binding.modifiers.red) ? '12px' : '16px';
+
+  },
+  inserted(element, binding, vNode) {
+    //access to the parent Node
+  },
+  update(element, binding, vNode,oldNode) {
+    console.log('update');
+  },
+  componentUpdated(element, binding, vNode,oldNode){
+    console.log('updated');
+  },
+  unbind(element, binding, vNode,oldNode){
+    console.log('unbind');
+  }
 });
 
 new Vue({
